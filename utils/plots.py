271361c2951791bdefcdef87bfa4ae8c50b471f5
kalezhi <93209907+kalezhi@users.noreply.github.com>
2024-08-06 19:25:47 +0200

    plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -510,14 +510,18 @@ def plot_results(file='path/to/results.csv', dir=''):
             for i, j in enumerate([1, 2, 3, 4, 5, 8, 9, 10, 6, 7]):
                 y = data.values[:, j].astype('float')
                 # y[y == 0] = np.nan  # don't show zero values
-                ax[i].plot(x, y, marker='.', label=f.stem, linewidth=2, markersize=8)
-                ax[i].set_title(s[j], fontsize=12)
+                ax[i].plot(x, y, marker='.', label=f.stem, linewidth=4, markersize=10)
+                ax[i].set_title(s[j], fontsize=14)
+                plt.setp(ax[i].get_xticklabels(), fontsize=14)
+                plt.setp(ax[i].get_yticklabels(), fontsize=14)
                 # if j in [8, 9, 10]:  # share train and val loss y axes
                 #     ax[i].get_shared_y_axes().join(ax[i], ax[i - 5])
         except Exception as e:
             LOGGER.info(f'Warning: Plotting error for {f}: {e}')
     ax[1].legend()
-    fig.savefig(save_dir / 'results.png', dpi=200)
+    fig.supxlabel('epochs', fontsize=16)    
+    plt.tight_layout()
+    fig.savefig(save_dir / 'results.png', dpi=300)
     plt.close()
 
 
